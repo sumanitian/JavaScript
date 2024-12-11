31c9f94d34bbfc5572385c90c418696419b49d1d
sumanitian <sumanprasad26042001@gmail.com>
2024-12-11 23:44:25 +0530

    Comparison

---
+++ Comparisons/index.js
@@ -0,0 +1,24 @@
+console.log(2 > 1); // true
+console.log(2 >= 1); // true
+console.log(2 < 1); // false
+console.log(2 == 1); // false
+console.log(2 != 1);// true
+
+console.log("2" > 1); // true
+console.log("02" > 1); // true
+
+console.log(null > 0); // false
+console.log(null == 0); //false
+console.log(null >= 0); //true
+
+//The reason is that an equality check == and comaprisons ><>= <= work differently.
+//Comparisons convert null to a number, treating it as 0.
+// That's why (3) null >= 0 is true and (1) null > 0 is false.
+
+
+console.log(undefined == 0); // false
+console.log(undefined > 0); // false
+console.log(undefined < 0); // false
+
+// using strict check
+console.log("2" === 2); // false
