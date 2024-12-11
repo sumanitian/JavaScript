a80c0fe413101b32e96f7faa83aad2f8f998a034
sumanitian <sumanprasad26042001@gmail.com>
2024-12-12 00:31:32 +0530

    updated the variable folder

---
+++ 1_Variables/index.js
@@ -0,0 +1,22 @@
+let a = 5;
+let name = "suman";
+let status = true;
+
+// a, name, status all are variable name
+
+// const num = 12;
+// num = 13; 
+// throw error: Uncaught TypeError: Assignment to Constant Variable.
+
+const accountId = 142;
+let accountEmail = "xyz@gmail.com";
+var accountPassword = "12345";
+accountCity='xyz' // this is not a good practice
+
+let accountstate; //thius will give undefined
+
+accountEmail = "yxz@gmail.com";
+accountPassword = "212121243343";
+accountCity = "yxc"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])
