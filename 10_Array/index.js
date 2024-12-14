d764711245654b3b084c61c098c09891d096450b
sumanitian <sumanprasad26042001@gmail.com>
2024-12-14 21:33:44 +0530

    more on array

--- 10_Array/index.js
+++ 10_Array/index.js
@@ -45,3 +45,44 @@ console.log("C ", myArr) // C  [ 0, 4, 5, 6 ]
 console.log(myn2); // [ 1, 2, 3 ]
 
 // splice manuplates the original array
+
+const marvel_heros = ["thor", "ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros) // [ 'thor', 'ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] ]
+// // problem is array inside array
+// // this also proves that array takes any data. This has taken array as a data.
+// // push -> it pushes the data on the existing array
+// console.log(marvel_heros[3]) // [ 'superman', 'flash', 'batman' ]
+// console.log(marvel_heros[3][1]) // flash
+
+const allHeros = marvel_heros.concat(dc_heros)
+console.log(allHeros); // [ 'thor', 'ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+
+// concat actually returns the new array.
+
+// more easy way is spread operator
+
+const all_new_heros = [...marvel_heros, ...dc_heros]
+console.log(all_new_heros) // [ 'thor', 'ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+
+// Rarely
+const another_array = [1, 2, 3, [4, 5, 6] ,7, [6, 7, [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array) // [ 1, 2, 3, 4, 5, 6, 7, 6, 7, 4, 5 ]
+
+console.log(Array.isArray('Suman')) // false
+console.log(Array.from('Suman')) // [ 'S', 'u', 'm', 'a', 'n' ]
+
+console.log(Array.from({name: 'Suman'})) // [] what to make array key or string? Interestsing
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3)) // [ 100, 200, 300 ]
+
+
