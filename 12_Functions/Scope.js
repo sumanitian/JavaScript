a57c76b1b28160f7da57d9b62d7a7ff1f25e2ff8
sumanitian <sumanprasad26042001@gmail.com>
2024-12-15 20:59:35 +0530

    hoisting

--- 12_Functions/Scope.js
+++ 12_Functions/Scope.js
@@ -22,3 +22,69 @@ if (true){
 // {} inside this value should not go out
 
 console.log(a) // 300
+
+// function one(){
+//     const username = "suman"
+
+//     function two(){
+//         const website = "youtube"
+//         console.log(username);
+//     }
+//     console.log(website); // error line by line execution so two never gets executed.
+    
+//     two(); // this never get executed.
+// }
+
+// one()
+
+function one(){
+    const username = "suman"
+
+    function two(){
+        const website = "youtube"
+        console.log(username); // suman
+    }
+    // console.log(website); 
+    
+    two();
+}
+
+// one()
+
+
+
+if (true) {
+    const username = "suman"
+    if (username === "suman") {
+        const website = "youtube"
+        console.log(username + website);
+    }
+    //console.log(website); // error because of scope
+}
+//console.log(username); // error because of scope
+
+
+// Hoisting
+
+// function addone(num){
+//     return num + 1
+// }
+// console.log(addone(5)) // 6
+
+
+// const addTwo = function(num){
+//     return num + 2
+// }
+// console.log(addTwo(5)) // 7
+
+
+console.log(addone(5)) // 6
+function addone(num){
+    return num + 1
+}
+
+console.log(addTwo(5)) // ReferenceError: Cannot access 'addTwo' before initialization
+// this error how you declare the functions.in this we had declared and stored in a variable.
+const addTwo = function(num){
+    return num + 2
+}
