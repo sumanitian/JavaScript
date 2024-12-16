b5c32803014869a5ee6ddb1115691fc6e3e322ac
sumanitian <sumanprasad26042001@gmail.com>
2024-12-16 22:34:37 +0530

    if condition

---
+++ 5_Conditions/Controlflow.js
@@ -0,0 +1,22 @@
+// if
+// = assignment
+// == check
+// != not equal
+// === strict checking it checks type as well.
+
+// if (2 == "2"){
+//     console.log("Executed")
+// }
+
+// if (2 === "2"){
+//     console.log("Executed")
+// }
+
+const temperature = 6
+if(temperature < 10){
+    console.log("less than 10") // less than 10
+}
+console.log("temperature is greater than 10"); // temperature is greater than 10
+
+// this will get executed as it is not in the scope of if
+
