39f8a610f4f1bf59b03b8480c972a7c7f6bd3256
sumanitian <sumanprasad26042001@gmail.com>
2024-12-16 23:18:03 +0530

    Nullish Coalescing Operator and Ternary Operator

--- 5_Conditions/TruthyFalsy.js
+++ 5_Conditions/TruthyFalsy.js
@@ -35,3 +35,24 @@ if(Object.keys(emptyObj).length === 0){
 // true
 // 0 == ''
 // true
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+// val1 = null ?? 10 ?? 20
+console.log(val1); // 5
+console.log(val1); // 10
+console.log(val1); // 15
+console.log(val1); // 10
+
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80"): console.log("more than 80")
+
