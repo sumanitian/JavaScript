6436e687128e32256159de11f7cd5714ea61eaea
sumanitian <sumanprasad26042001@gmail.com>
2024-12-17 13:30:52 +0530

    break in loop

--- 5_Conditions/Iterations.js
+++ 5_Conditions/Iterations.js
@@ -27,3 +27,22 @@ for (let i = 0; i <= 10; i++) {
         console.log(i + '*' + j + ' = ' + i*j);    
     }
 }
+
+let myArray = ["flash", "batman", "suprman"]
+console.log(myArray.length);
+
+for(let index = 0; index < myArray.length; index++){
+    const element = myArray[index];
+    console.log(element);
+}
+
+// break and continue
+
+for (let index = 2; index <= 20; index++) {
+    if (index == 5) {
+        console.log('Detected 5');
+        break;
+    }
+    console.log(`Value of i is ${index}`);
+    
+}
