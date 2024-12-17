5a9314efbf1be8b55f5450a2b19e132383ed73e8
sumanitian <sumanprasad26042001@gmail.com>
2024-12-17 13:16:56 +0530

    for loop

---
+++ 5_Conditions/Iterations.js
@@ -0,0 +1,22 @@
+// for
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    console.log(element);
+}
+//console.log(element); // error elements will not be accessible.
+
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    if (element == 5){
+        console.log("5 is best number");
+    }
+    console.log(element);
+}
+
+
