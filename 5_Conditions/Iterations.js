1f43f5a196019db1ed24c0d7fb7f94ffc2661514
sumanitian <sumanprasad26042001@gmail.com>
2024-12-17 13:32:44 +0530

    continue in loop

--- 5_Conditions/Iterations.js
+++ 5_Conditions/Iterations.js
@@ -46,3 +46,12 @@ for (let index = 2; index <= 20; index++) {
     console.log(`Value of i is ${index}`);
     
 }
+
+for (let index = 2; index <= 20; index++) {
+    if (index == 5) {
+        console.log('Detected 5');
+        continue
+    }
+    console.log(`Value of i is ${index}`);
+    
+}
