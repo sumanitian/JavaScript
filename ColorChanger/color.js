253e0674071f28d69fea60ba770f5e76c9a6d343
sumanitian <sumanprasad26042001@gmail.com>
2024-12-20 23:14:15 +0530

    completed

--- ColorChanger/color.js
+++ ColorChanger/color.js
@@ -9,5 +9,17 @@ buttons.forEach(function(button){
         if(e.target.id === 'grey'){
             body.style.backgroundColor = e.target.id
         }
+        if(e.target.id === 'white'){
+            body.style.backgroundColor = e.target.id
+        }
+        if(e.target.id === 'blue'){
+            body.style.backgroundColor = e.target.id
+        }
+        if(e.target.id === 'yellow'){
+            body.style.backgroundColor = e.target.id
+        }
+        if(e.target.id === 'purple'){
+            body.style.backgroundColor = e.target.id
+        }
     })
 });
