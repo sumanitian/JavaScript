fc7581c0e3e7b8a9d33100c0b8a215e979634380
sumanitian <sumanprasad26042001@gmail.com>
2024-12-28 23:07:35 +0530

    Closures

---
+++ 24_Closures/closures.js
@@ -0,0 +1,6 @@
+/* 
+    A closure is the combination of a function bundled together (enclosed)
+    with references to its surrounding state (the lexical environment)
+
+    a closure gives you access to an outer function's scope from an inner function.
+*/
