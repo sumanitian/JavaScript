aaa5749b70b96a5203d9f67a14723a622312c3d0
sumanitian <sumanprasad26042001@gmail.com>
2024-12-28 21:47:40 +0530

    getter_setter

--- 23_OOP/getter_setter.js
+++ 23_OOP/getter_setter.js
@@ -4,12 +4,21 @@ class User {
         this.password = password
     }
     // once you define getter then define setter also
+
+    get email(){
+        return this._email.toUpperCase()
+    }
+
+    set email(value){
+        this._email = value
+    }
+
     get password(){
-        return this.password.toUpperCase()
+        return this._password.toUpperCase()
     }
 
     set password(value){
-        this.password = value
+        this._password = value
     }
 }
 
