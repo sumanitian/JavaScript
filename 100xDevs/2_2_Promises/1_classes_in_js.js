908d819b1008798edc94507e15756843b26d1676
sumanitian <sumanprasad26042001@gmail.com>
2025-05-22 15:46:50 +0530

    defaultclasses

--- 100xDevs/2_2_Promises/1_classes_in_js.js
+++ 100xDevs/2_2_Promises/1_classes_in_js.js
@@ -96,3 +96,7 @@ map.set('age', 30);
 const firstName = map.get('name');
 console.log(firstName); 
 
+const d = new Date(); // created a new object of the Date class
+
+console.log(d.getFullYear()); // I called a function on the object
+
