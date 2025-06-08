f21d1fc4807a4a7ff4d92c73fddad9ca3b54bf26
sumanitian <sumanprasad26042001@gmail.com>
2025-06-08 20:36:50 +0530

    updated the singletonandconstructor file

--- 11_Object/2SingletonAndConstructor.js
+++ 11_Object/2SingletonAndConstructor.js
@@ -62,10 +62,10 @@ console.log(obj3) // { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
 
 // users[1].email
 
-console.log(tinderUser);
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+console.log(tinderUser);  // { id: '123abc', name: 'Sammy', isLoggedIn: false }
+console.log(Object.keys(tinderUser)); // [ 'id', 'name', 'isLoggedIn' ]
+console.log(Object.values(tinderUser)); // [ '123abc', 'Sammy', false ]
+console.log(Object.entries(tinderUser)); // [ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ]
 
 // you can apply loop on this
 
